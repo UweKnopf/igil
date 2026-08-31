39b7199bbddb61c6df78d823c02e297bc9fe561b
Uwe_Knopf <bob@fake.com>
2026-08-31 14:20:01 +0200

    Add: Base Packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxthub/core',
+    '@nuxtjs/seo',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxt/devtools',
+    '@nuxt/eslint'
+  ],
+  hub: {
+    //Config options for hub
+  }
 })
